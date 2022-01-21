1d21120171c66b0ed264e6d9ac5e2408a45c097a
OndigoWendy <ondigowendy004@gmail.com>
2022-01-21 18:26:17 +0300

    Add restful link for single employee data

--- src/main/java/com/ondigo/payroll/PayrollApplication.java
+++ src/main/java/com/ondigo/payroll/PayrollApplication.java
@@ -2,7 +2,12 @@ package com.ondigo.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @SpringBootApplication
@@ -10,5 +15,16 @@ public class PayrollApplication {
 
 	public static void main(String... args) {
 		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+	@GetMapping("/employees/{id}")
+	EntityModel<Employee> one(@PathVariable Long id) {
+
+		Employee employee = repository.findById(id) //
+				.orElseThrow(() -> new EmployeeNotFoundException(id));
+
+		return EntityModel.of(employee, //
+				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
 	}
 }
