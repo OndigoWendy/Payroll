9784202f58c8b3ad6578751338592ce8467f89f1
OndigoWendy <ondigowendy004@gmail.com>
2022-01-25 20:12:01 +0300

    Getting aggregate root resource using the assembler

--- src/main/java/com/ondigo/payroll/PayrollApplication.java
+++ src/main/java/com/ondigo/payroll/PayrollApplication.java
@@ -35,13 +35,22 @@ public class PayrollApplication {
 
 	}
 
+//	@GetMapping("/employees")
+//	CollectionModel<EntityModel<Employee>> all() {
+//
+//		List<EntityModel<Employee>> employees = repository.findAll().stream()
+//				.map(employee -> EntityModel.of(employee,
+//						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+//						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+//				.collect(Collectors.toList());
+//
+//		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+//	}
 	@GetMapping("/employees")
 	CollectionModel<EntityModel<Employee>> all() {
 
-		List<EntityModel<Employee>> employees = repository.findAll().stream()
-				.map(employee -> EntityModel.of(employee,
-						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+		List<EntityModel<Employee>> employees = repository.findAll().stream() //
+				.map(assembler::toModel) //
 				.collect(Collectors.toList());
 
 		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
